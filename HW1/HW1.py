4a1f1281ac3de96eda2ca803e210317ca0d36508
coolis <wayne.liwen@gmail.com>
2014-10-01 23:16:26 -0400

    Task 1 and Task 2 completed

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,7 +1,17 @@
-import numpy
+﻿# -*- coding: utf-8-sig -*-
+import pylab
+import os
 import math
+import numpy
 
-x = [9,7,3,5]
+dir = os.path.dirname(__file__)
+file_name = "./Data/accel.csv"
+
+def graph_accel(signal):
+    pylab.ion()
+    pylab.figure()
+    pylab.plot(signal)
+    pylab.ioff()
 
 #haar transform
 #base vector
@@ -51,6 +61,60 @@ def inverse_haar(x):
 
     return inputVector
 
-print haar(x)
+#trim the data to the largest 2 to the n length array
+#@param array or numpy array
+#@return numpy array
+def trim_n2(data):
+    index = 1
+    base = math.pow(2, index)
+    while (len(data) / base > 2):
+        index = index + 1
+        base = math.pow(2, index)
+    return data[0:int(math.floor(base))]
 
+
+#smooth the haar signal by changing all the first n to the d bits to 0
+def smooth(data, d=1):
+    data_copy = list(data)
+    n = len(data_copy)
+    degree = n / int(math.floor(pow(2, d)))
+    if n <= degree:
+        return data_copy
+    for i in range(degree-1, n):
+        data_copy[i] = 0
+    return data_copy
+
+#find the largest n positive elements and the smallest n negagive elements
+#@param: data: numpy array, the array to find
+#        n: the largest n elements to return
+#@return: the list of the n positive and negative elements
+def find_edges(data, n=5):
+    data.sort()
+    return data[:n], data[-n:]
+
+if __name__ == '__main__':
+    f = open(os.path.join(dir, file_name), 'r')
+
+    # Get just the displacement in the x coordinate
+    x = []
+    for line in f:
+        x.append(float(line.split(',')[6]))
+
+    haar_x = haar(trim_n2(x))
+    positive_largest, negative_largest = find_edges(haar_x[len(haar_x)/2:], 5)
+    print positive_largest, negative_largest
+
+    haar_x1 = smooth(haar_x, 1)
+    haar_x2 = smooth(haar_x, 2)
+    haar_x3 = smooth(haar_x, 3)
+    haar_x4 = smooth(haar_x, 4)
+
+    #Graph it, and save figure as a .png
+    graph_accel(x)
+    graph_accel(haar_x)
+    graph_accel(inverse_haar(haar_x1))
+    graph_accel(inverse_haar(haar_x2))
+    graph_accel(inverse_haar(haar_x3))
+    graph_accel(inverse_haar(haar_x4))
+    pylab.show()
 
